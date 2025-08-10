3087ce9c8e60f3c5e112253dbd572e9218834895
Sad Gabi <sadgabi20@gmail.com>
2025-08-10 15:00:17 -0600

    fix: __dirname declaration
    - Declared __dirname const in main

--- bin/chocola.js
+++ bin/chocola.js
@@ -2,7 +2,10 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const [, , command, projectName] = process.argv;
 
 if (command === 'create' && projectName) {
