3e9c18999aa306d63f4f594eeac918f7ba4250de
Sad Gabi <sadgabi20@gmail.com>
2025-08-10 13:16:44 -0600

    add: chocola.js
    - Added bin/chocola.js script

---
+++ bin/chocola.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const [, , command, projectName] = process.argv;
+
+if (command === 'create' && projectName) {
+  const targetPath = path.resolve(process.cwd(), projectName);
+
+  if (fs.existsSync(targetPath)) {
+    console.error(`Error: Directory ${projectName} already exists.`);
+    process.exit(1);
+  }
+
+  // COPY TEMPLATE
+  copyFolderRecursiveSync(path.resolve(__dirname, '../templates/base'), targetPath);
+
+  console.log(`Project ${projectName} created!`);
+  console.log(`Go to the directory with: cd ${projectName}`);
+  console.log('Install dependencies with: npm install');
+  console.log('And run with: npm run dev');
+
+} else {
+  console.log('Use: chocola create <project-name>');
+}
+
+function copyFolderRecursiveSync(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyFolderRecursiveSync(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
